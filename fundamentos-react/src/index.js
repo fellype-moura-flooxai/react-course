2620bc4ca3f47e5fb33f897c8a0ca6b5e9403465
fellype-moura-flooxai <fellype.moura.flooxai@gmail.com>
2025-04-15 11:55:22 -0300

    feat: learning to react with tests

---
+++ fundamentos-react/src/index.js
@@ -0,0 +1,12 @@
+import ReactDOM from 'react-dom/client'
+import {React} from 'react';
+
+const tag = <strong>Olá React</strong>
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+root.render(
+    <div>
+        { tag }
+        </div>
+  )
