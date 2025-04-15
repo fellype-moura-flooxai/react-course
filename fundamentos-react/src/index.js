ec49fc199545451422346c2adeda9a141df59f8f
fellype-moura-flooxai <fellype.moura.flooxai@gmail.com>
2025-04-15 13:08:28 -0300

    feat: receive properties in component

--- fundamentos-react/src/index.js
+++ fundamentos-react/src/index.js
@@ -3,11 +3,16 @@ import ReactDOM from 'react-dom/client'
 import React from 'react';
 
 import Primeiro from './components/basicos/Primeiro';
+import ComParametro from './components/basicos/ComParametro';
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
 root.render(
-    <div>
+    <div id='app'>
        <Primeiro></Primeiro> 
-        </div>
+       <ComParametro
+       titulo='Situação do Aluno'
+       aluno='Pedro Silva' 
+       nota={9.3} /> 
+    </div>
   )
