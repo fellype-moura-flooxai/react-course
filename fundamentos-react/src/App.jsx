dd392ab621f73d3bd147be22fc19f6d70afc0afb
fellype-moura-flooxai <fellype.moura.flooxai@gmail.com>
2025-04-16 10:58:39 -0300

    feat: passing parent properties to children and clone elements

--- fundamentos-react/src/App.jsx
+++ fundamentos-react/src/App.jsx
@@ -1,38 +1,47 @@
+import "./App.css";
 import React from "react";
 
 import Card from "./components/layout/Card";
+
+import Familia from "./components/basicos/Familia";
+import FamiliaMembro from "./components/basicos/FamiliaMembro";
 import Aleatorio from "./components/basicos/Aleatorio";
 import ComParametro from "./components/basicos/ComParametro";
 import Fragmento from "./components/basicos/Fragmento";
 import Primeiro from "./components/basicos/Primeiro";
 
 export default () => (
-  <div id="App">
+  <div className="App">
     <h1>fundamentos reacts </h1>
 
-    <div className="cards">
+    <div className="Cards">
+      <Card titulo="#5 - Componente com Filhos" color="#00c8f8">
+        <Familia sobrenome="Soares">
+          <FamiliaMembro nome="Pedro" />
+          <FamiliaMembro nome="Ana" />
+          <FamiliaMembro nome="Gustavo" />
+        </Familia>
+      </Card>
 
+      <Card titulo="#4 - Desafio Aleatorio" color="#fa6900">
+        <Aleatorio min={1} max={60} />
+      </Card>
+
+      <Card titulo="#3 - Fragmento" color="#e94c6f">
+        <Fragmento />
+      </Card>
+
+      <Card titulo="#2 - Com Parametro" color="#e8b71a">
+        <ComParametro
+          titulo="Situação do Aluno"
+          aluno="Pedro Silva"
+          nota={8.7}
+        />
+      </Card>
+
+      <Card titulo="#1 - Primeiro Componente" color="#588c73">
+        <Primeiro></Primeiro>
+      </Card>
     </div>
-
-    <Card titulo="#4 - Desafio Aleatorio">
-    <Aleatorio min={1} max={60} />
-      </Card>
-
-    <Card titulo="#3 - Fragmento">
-    <Fragmento />
-      </Card>
-
-    <Card titulo="#2 - Com Parametro">
-    <ComParametro 
-    titulo="Situação do Aluno" 
-    aluno="Pedro Silva" 
-    nota={8.7} 
-    />
-    </Card>
-
-    <Card titulo="#1 - Primeiro Componente">
-    <Primeiro></Primeiro>
-    </Card>
-
   </div>
 );
--- fundamentos-react/src/components/layout/Card.jsx
+++ fundamentos-react/src/components/layout/Card.jsx
@@ -2,8 +2,14 @@ import "./Card.css"
 import React from "react";
 
 export default (props) => {
-  return (
-    <div className="Card">
+    
+    const cardStyle = {
+        backgroundColor: props.color || '#f00',
+        borderColor: props.color || '#f00',
+    }
+
+    return (
+    <div className="Card" style={cardStyle}>
       <div className="Title">{props.titulo}</div>
       <div className="Content">
         {props.children}
