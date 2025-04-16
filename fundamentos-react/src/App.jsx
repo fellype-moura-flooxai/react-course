536dc449b69ad0e7361b710f5474a794910b7252
fellype-moura-flooxai <fellype.moura.flooxai@gmail.com>
2025-04-16 09:55:08 -0300

    feat: import google font and organize exercises and cards

--- fundamentos-react/src/App.jsx
+++ fundamentos-react/src/App.jsx
@@ -1,20 +1,35 @@
 import React from "react";
 
+import Card from "./components/layout/Card";
 import Aleatorio from "./components/basicos/Aleatorio";
-import Primeiro from "./components/basicos/Primeiro";
 import ComParametro from "./components/basicos/ComParametro";
 import Fragmento from "./components/basicos/Fragmento";
+import Primeiro from "./components/basicos/Primeiro";
 
 export default () => (
   <div id="app">
-    <h1>fundamentos reacts 2</h1>
-    <Aleatorio min={1} max={60} />
+    <h1>fundamentos reacts </h1>
+
+    <Card titulo="#4 - Desafio Aleatorio">
+    <Fragmento />
+      </Card>
+
+    <Card titulo="#3 - Fragmento">
+    <Aleatorio/>
+      </Card>
+
+    <Card titulo="#2 - Com Parametro">
     <Fragmento />
     <ComParametro 
     titulo="Situação do Aluno" 
     aluno="Pedro Silva" 
     nota={8.7} 
     />
+    </Card>
+
+    <Card titulo="#1 - Primeiro Componente">
     <Primeiro></Primeiro>
+    </Card>
+
   </div>
 );
