c2c825660742e64b79d1744caeedee15f7129cd5
fellype-moura-flooxai <fellype.moura.flooxai@gmail.com>
2025-04-18 12:30:41 -0300

    feat: Applying style

--- navegacao/src/index.js
+++ navegacao/src/index.js
@@ -1,5 +1,7 @@
+import './index.css'
 import { createRoot } from 'react-dom/client';
 import React from 'react'
+
 import App from './views/App';
 
 const root = createRoot(document.getElementById("root"));
--- navegacao/src/views/examples/About.jsx
+++ navegacao/src/views/examples/About.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const About = props => (
     <div className="About">
-        <h1>SObre</h1>
+        <h1>Sobre</h1>
         <p>O nosso sistema fdoi criado...</p>
     </div>
 )
