b131edfe97fe473db04a8d0e54304f9f1547ed94
fellype-moura-flooxai <fellype.moura.flooxai@gmail.com>
2025-04-18 12:10:21 -0300

    feat: created structure

--- navegacao/src/index.js
+++ navegacao/src/index.js
@@ -1,5 +1,6 @@
 import { createRoot } from 'react-dom/client';
 import React from 'react'
+import App from './views/App';
 
 const root = createRoot(document.getElementById("root"));
-root.render(<h1>App</h1>);
+root.render(<App />,);
---
+++ navegacao/src/views/examples/About.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const About = props => (
+    <div className="About">
+        <h1>SObre</h1>
+        <p>O nosso sistema fdoi criado...</p>
+    </div>
+)
+
+export default About
