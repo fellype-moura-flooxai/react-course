8ee03a7d51b9cdf8397291d1ee98e34ba84f86fc
fellype-moura-flooxai <fellype.moura.flooxai@gmail.com>
2025-04-23 10:41:58 -0300

    feat: custom hooks

--- hooks/src/views/examples/UseReducer.jsx
+++ hooks/src/views/examples/UseReducer.jsx
@@ -1,32 +1,8 @@
 import React, { useReducer } from 'react'
 import PageTitle from '../../components/layout/PageTitle'
 
-const initialState = {
-    cart: [],
-    products: [],
-    user: null,
-    // foco...
-    number: 0
-}
-
-function reducer(state, action) {
-    switch (action.type) {
-        case 'numberAdd2':
-            return { ...state, number: state.number + 2 }
-        case 'numberMulti7':
-            return { ...state, number: state.number * 7 }
-        case 'numberDiv25':
-            return { ...state, number: state.number / 25 }
-        case 'numberInt':
-            return { ...state, number: parseInt(state.number) }
-        case 'numberAddN':
-            return { ...state, number: state.number + action.payload }
-        case 'login':
-            return { ...state, user: { name: action.payload } }
-        default:
-            return state
-    }
-}
+import { initialState, reducer } from '../../store'
+import { numberAdd2, login } from '../../store/actions'
 
 const UseReducer = (props) => {
     const [state, dispatch] = useReducer(reducer, initialState)
@@ -48,11 +24,13 @@ const UseReducer = (props) => {
                 <div>
                     <button
                         className="btn"
-                        onClick={() => dispatch({ type: 'login', payload: 'Maria' })}
-                    >Login</button>
+                        onClick={() => login(dispatch, 'fellype')}
+                    >
+                        Login
+                        </button>
                     <button
                         className="btn"
-                        onClick={() => dispatch({ type: "numberAdd2" })}>
+                        onClick={() => numberAdd2(dispatch)}>
                         +2</button>
                     <button
                         className="btn"
